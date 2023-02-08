ddb8b1be76b8cc6a4ac3a0d53262fc7788dd2cba
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-08 10:58:47 +0300

    Задание 1. В процессе2

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -77,6 +77,12 @@ int GetMinMax(string minOrMax)
     return result;
 }
 
+int[,] SortArray2d(int[,] oldArray2d)
+{
+	int[,] sortArray2d = new int[oldArray2d.GetLength(0), oldArray2d.GetLength(1)];
+
+	return sortArray2d;
+}
 
 Console.WriteLine("Программа создаёт случайный двумерный массив.");
 Console.WriteLine("После программа сортирует каждую строку массива по убыванию и создаёт новый массив");
@@ -87,8 +93,10 @@ int min = GetMinMax("минимума");
 int max = GetMinMax("максимума");
 
 int[,] array2d = CreateRandom2DArray(m, n, min, max);
+Console.WriteLine("");
 Console.WriteLine("Сгенерированный массив:");
 PrintArray2d(array2d);
-
 int[,] sortArray2d = SortArray2d(array2d);
-PrintArray2d(array2d);
+Console.WriteLine("");
+Console.WriteLine("Отсортированный массив:");
+PrintArray2d(sortArray2d);
