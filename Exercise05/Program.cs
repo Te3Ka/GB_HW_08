91c0041060238d7c1dd5916bf0489b09df8a4177
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-08 15:35:21 +0300

    Задание 5. В процессе

---
+++ Exercise05/Program.cs
@@ -0,0 +1,74 @@
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+//Метод с указанием автора
+void Author()
+{
+    Console.WriteLine();
+    Console.WriteLine("Программа создана Te3Ka_PaynE.");
+    Console.WriteLine("E-mail: Mnement4813@yandex.ru");
+    Console.WriteLine();
+}
+
+// Печать двумерного массива на экран
+void PrintArray2d(int[,] arrayPrint)
+{
+    for (int i = 0; i < arrayPrint.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < arrayPrint.GetLength(1); j++)
+        {
+            if (j == (arrayPrint.GetLength(1) - 1))
+                Console.WriteLine(String.Format("{0,5}]", arrayPrint[i, j]));
+            else
+                Console.Write(String.Format("{0,5} ", arrayPrint[i, j]));
+        }
+    }
+}
+
+int[,] CreateSpiral2DArray(int m, int n)
+{
+	int[,] array2d = new int[m, n];
+	int i = 0, j = 0;
+	int count = 1;
+	int direction = 1;
+	bool correct = true;
+	while (correct)
+	{
+		
+	}
+	return array2d;
+}
+
+//Запрос на количество строк или столбцов в массиве
+int GetRowsOrColumnsLengthArray2d(string columnsOrRows)
+{  
+    Console.Write($"Сколько {columnsOrRows} будет в массиве? ");
+    int.TryParse(Console.ReadLine(), out int num);
+    while (num <= 0)
+    {
+        Console.WriteLine("Число меньше 1! Массив не может быть создан. Введите заново: ");
+        int.TryParse(Console.ReadLine(), out num);
+    }
+    return num;
+}
+
+Console.WriteLine("Программа создаёт двумерный массив, который заполняется спирально (начинается от 1).");
+Console.WriteLine("Задайте параметры массива:");
+int m = GetRowsOrColumnsLengthArray2d("строк");
+int n = GetRowsOrColumnsLengthArray2d("столбцов");
+int[,] array2d = CreateSpiral2DArray(m, n);
+PrintArray2d(array2d);
+
+Author();
